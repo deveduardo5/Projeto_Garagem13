56592d81d11480f5ae7830ef1337b50821ebe56b
EDUARDO ALCANTARA DE SOUZA <eduardo.asouza23@senacsp.edu.br>
2025-04-28 19:16:01 -0300

    Atualizando Projeto

--- banco-de-dados/DataBase.cs
+++ banco-de-dados/DataBase.cs
@@ -1,75 +1,66 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 
+//namespace garagem13.banco_de_dados
+//{
+//    internal class DataBase
+//    {
+//        private static readonly string ConnectionString = "datasource=localhost;username=root;password=;database=garagem13sql;";
+
+//        public static MySqlConnection GetConnection/*CriarConexao*/()
+//        {
+//            return new MySqlConnection(ConnectionString);
+//        }
+
+//        internal MySqlConnection AbrirConexao()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        internal void FecharConexao()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
+
 namespace garagem13.banco_de_dados
 {
     internal class DataBase
     {
         private static readonly string ConnectionString = "datasource=localhost;username=root;password=;database=garagem13sql;";
 
-        public static MySqlConnection GetConnection/*CriarConexao*/()
+        private MySqlConnection connection;
+
+        public static MySqlConnection GetConnection()
         {
             return new MySqlConnection(ConnectionString);
         }
 
-        internal MySqlConnection AbrirConexao()
+        public MySqlConnection AbrirConexao()
         {
-            throw new NotImplementedException();
+            if (connection == null)
+            {
+                connection = new MySqlConnection(ConnectionString);
+            }
+
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+
+            return connection;
         }
 
-        internal void FecharConexao()
+        public void FecharConexao()
         {
-            throw new NotImplementedException();
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+                connection.Dispose();
+                connection = null;
+            }
         }
     }
 }
-
-//namespace garagem13.banco_de_dados
-//{
-//    internal class DataBase
-//    {
-//        // Aqui guardamos as informações para conectar no banco de dados
-//        private static readonly string ConnectionString = "datasource=localhost;username=root;password=;database=garagem13sql;";
-
-//        // Esse objeto representa a conexão com o banco
-//        private MySqlConnection connection;
-
-//        // Método para criar uma nova conexão
-//        public static MySqlConnection CriarConexao()
-//        {
-//            // Cria e devolve uma nova conexão usando a ConnectionString
-//            return new MySqlConnection(ConnectionString);
-//        }
-
-//        // Método para abrir a conexão com o banco
-//        public MySqlConnection AbrirConexao()
-//        {
-//            // Se a conexão ainda não existe, cria uma nova
-//            if (connection == null)
-//            {
-//                connection = new MySqlConnection(ConnectionString);
-//            }
-
-//            // Se a conexão está fechada, abre ela
-//            if (connection.State != System.Data.ConnectionState.Open)
-//            {
-//                connection.Open();
-//            }
-
-//            return connection;
-//        }
-
-//        // Método para fechar a conexão com o banco
-//        public void FecharConexao()
-//        {
-//            // Se a conexão existe e está aberta, fecha e limpa ela
-//            if (connection != null && connection.State == System.Data.ConnectionState.Open)
-//            {
-//                connection.Close();    // Fecha a conexão
-//                connection.Dispose();  // Libera a memória usada
-//                connection = null;     // Limpa a variável
-//            }
-//        }
-//    }
-//}
 
--- obj/Debug/net8.0-windows/garagem13.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/garagem13.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("garagem13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da30dabb5f5e5f478f638dd7e59a9e96009f32c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ab2b94d13147faeb22aa1ba536755f1e794de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("garagem13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("garagem13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
